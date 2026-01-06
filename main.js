e0a60d2e6928f2052121ae1ed81bb8a39995529b
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-06 23:16:31 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+//Run code
+console.log("Hello everyone!");
+
+// Variable 
+var name;
+
+name = "john";
+
+console.log(name);
+
+//Override
+name = "robin";
+
+console.log(name);
+
+// Constent 
+ const age = 20;
+
+ console.log(age);
+
