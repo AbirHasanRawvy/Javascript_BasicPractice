92b89f30970f98d11c0852382e79b1ee2bf0fe54
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-14 00:55:33 +0600

    All the operators are discuss here.
    Here all the operators are discussed practice basic for me. 
    1. Arethremetic Operator,
    2. Assignment Operator,
    3. Comparison Operator,
    4. Logical Operator,
    5. Unary and ternary Operator.

--- baup.js
+++ baup.js
@@ -6,6 +6,7 @@ console.log("Hello World!");
 
 
  //Operators
+
         //Arethremetic Operator;
         let a = 10;
         let b = 5;
@@ -16,3 +17,31 @@ console.log("Hello World!");
         console.log(a / b);//division;
         console.log(a % b);//modulus;
        
+       //Assignment Operator;
+       a = a + 5;
+       console.log(a);
+       a -= 2;
+       console.log(a);
+       b*=5;
+       console.log(b);
+       a /= 13;
+       console.log(a);
+
+       //Comparison Operator;
+       
+       //Logical Operator;
+
+       //Unary and ternary Operator;
+       
+       //Unanry;
+       a++;
+       console.log(a);
+       b--;
+       console.log(b);
+       
+       //Ternary Operator;
+       let boyos = 20;
+       let isAdult = boyos > 18 ? "Adult" : "Minor";
+       console.log(isAdult);
+       let boyoss = 17;
+       let isAdults = boyoss < 18 ? "Not Adult" : "Minor";
