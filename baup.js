fcdbd77c3dddff2cecf031ca2add75599bfd1581
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-13 20:03:52 +0600

    Operators
    Here is only arethrematic Operator practice down.

--- baup.js
+++ baup.js
@@ -10,9 +10,9 @@ console.log("Hello World!");
         let a = 10;
         let b = 5;
 
-        console.log(a + b);
-        console.log(a - b);
-        console.log(a * b);
-        console.log(a / b);
-        console.log(a % b);
+        console.log(a + b);//additon;
+        console.log(a - b);//substraction;
+        console.log(a * b);//multification;
+        console.log(a / b);//division;
+        console.log(a % b);//modulus;
        
