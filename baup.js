f5ff9fdb686ec14109ec29e37486335864221af4
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-13 20:01:10 +0600

    Operators.
    Here is only wirte down arethremetic operators.

--- baup.js
+++ baup.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+//Object
+
+
+
+
+ //Operators
+        //Arethremetic Operator;
+        let a = 10;
+        let b = 5;
+
+        console.log(a + b);
+        console.log(a - b);
+        console.log(a * b);
+        console.log(a / b);
+        console.log(a % b);
+       
