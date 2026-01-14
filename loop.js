45deff149247ec7bfce4351bbf3efbc6606bcf45
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-14 13:59:52 +0600

    Loop
    Also discuss about the while loop, Do while loop, for loop with break and continue.

--- loop.js
+++ loop.js
@@ -6,9 +6,40 @@ for(let p=0; p<5; p++){
     console.log(p);
 
 }
-
+//same practice;
 for(let q=0; q<11; q++){
 
     console.log(q+". Hello Bangladesh");
 
 }
+
+//for loop with break;
+for(let r = 1; r<=10; r++){
+    console.log(r);
+    if(r===5){
+        break;
+    }//When r value will be 5 it will break;
+};
+
+//Continue 
+
+// for(let r = 1; r<=10; r++){
+//     console.log(r);
+//     if(r===5){
+//         Continue;
+//     }//When r value will be 5 it will break;
+//     if(r===10){
+//         break;
+//     };
+// };
+
+
+//While Loop;
+let o = 1;
+
+while(o <= 5){
+    console.log("Number is",o);
+    o++;
+};
+
+//Do While Loop;
