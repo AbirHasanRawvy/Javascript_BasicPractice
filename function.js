c4f4549a482fa74cea26497e248db2c3b0afa76e
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-14 23:01:55 +0600

    Function
    Here, discuss about the function basic practice.

---
+++ function.js
@@ -0,0 +1,6 @@
+//Function: When work on same think then use function;
+//In javascript function is a key word;
+function sayHello(){
+    console.log("Hello JS!");
+};
+sayHello();//call the function;
