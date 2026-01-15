9ce8d561a0371459434831779b885a8738f33970
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-15 15:17:36 +0600

    Function
    Here, practice on function basic. There have some topic is basic function, function parameter and function return with process.

--- function.js
+++ function.js
@@ -4,3 +4,19 @@ function sayHello(){
     console.log("Hello JS!");
 };
 sayHello();//call the function;
+
+//Function parameter: Parameter work like input for the function;
+function dd(num1, num2){//num1 and num2 parameter;
+
+    console.log(num1+num2);
+
+};
+dd(5,10);//It is a argument.If I am give the value it will be undefined. 
+
+//function return;
+ function addNumber(num3,num4){
+    return num3+num4;//return function;
+ }
+
+ let res = addNumber(10,5) + addNumber(50,60);//dfunction sub;
+ console.log(res);
