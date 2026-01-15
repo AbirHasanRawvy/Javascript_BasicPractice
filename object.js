189ac65d8366b60df6d72984be69929349936aa7
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-16 01:06:37 +0600

    Object
    Here, discuss and practice about object. First, practice on basic object then practice on object with mixed function thats called arguement.

--- object.js
+++ object.js
@@ -1,1 +1,30 @@
 //Object;
+let s = {
+    name: "Abir Hasan",
+    Agee: 25,
+    className:"Undergrade Student",
+    roll: 5,
+
+};
+
+s.subject="CSE"//add one more element;
+
+delete s.roll//delete roll element;
+
+console.log(s);//access all the information;
+console.log(s.name);//access only name,Its name is dot notation;
+
+
+//Function and Object mixed which function name is method;
+let s1 = {
+    name: "Abir Hasan",
+    Agee: 25,
+    className:"Undergrade Student",
+    roll: 5,
+    great:function(){//here, great function is a method;
+        console.log("hello");
+    },
+
+};
+s1.great();
+console.log(s1);
