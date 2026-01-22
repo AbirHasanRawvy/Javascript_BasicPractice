14bce3ca349476efd908d24a059895e2f3c24c41
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-22 22:27:12 +0600

    DateOfObject.
    Here, disscuss about the live date, month, year, minute and second.

--- dateObject.js
+++ dateObject.js
@@ -1,1 +1,26 @@
 //Live date find out;
+//today date;
+const now = new Date();
+console.log(now);
+
+//Manualy;
+const now1 = new Date("2001-01-20");
+console.log(now1);
+
+//year;
+console.log("Year: ", now.getFullYear());
+
+//month;
+console.log("Month: ", now.getMonth());
+
+//date;
+console.log("Date: ", now.getDate());
+
+//houre;
+console.log("Hour: ", now.getHours());
+
+//minute;
+console.log("Minute: ", now.getMinutes());
+
+//second;
+console.log("Second: ", now.getSeconds());
