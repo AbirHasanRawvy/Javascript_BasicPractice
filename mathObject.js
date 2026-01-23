b07661dd71630f7450391626b9fd0fc5ba807816
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-23 12:53:53 +0600

    MathObject.
    Here disscuss about 11 types math object on the basic practice.

---
+++ mathObject.js
@@ -0,0 +1,45 @@
+//math Object related all;
+
+//Math: Pi;
+let r15 = Math.PI;
+console.log(r15);
+
+//Math: round;
+let r16 = Math.round(4.6);
+console.log(r16);
+
+//Math: ceil;
+let r17 = Math.ceil(4.4);
+console.log(r17);
+
+//Math: floor: oposite of ceil;
+let r18 = Math.ceil(4.6);
+console.log(r18);
+
+//Squre root;
+let r19 = Math.sqrt(4);
+console.log(r19);
+
+//Power;
+let r20 = Math.pow(4,3);
+console.log(r20);
+
+//Possitive Number;
+let r21 = Math.abs(-34);
+console.log(r21);
+
+//Min;
+let r22 = Math.min(4.5,9,4,2);
+console.log(r22);
+
+//Max;
+let r23 = Math.max(3,6,7,10,2);
+console.log(r23);
+
+//Random;
+let r24 = Math.random();
+console.log(r24);
+
+//Random with solid number;
+let r25 = Math.floor(Math.random()*100) + 1;
+console.log(r25);
