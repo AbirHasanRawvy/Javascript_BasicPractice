11309ccdd4b0f9ab11088b347314a31b0dc25c4c
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-23 11:31:31 +0600

    DateObject.
    Here, much more date object practive on basic.

--- dateObject.js
+++ dateObject.js
@@ -24,3 +24,35 @@ console.log("Minute: ", now.getMinutes());
 
 //second;
 console.log("Second: ", now.getSeconds());
+
+
+//manualy input;
+const now2 = new Date();
+
+now2.setFullYear(2025);
+now2.setMonth(0);
+now2.setDate(23);
+
+console.log(now2);
+
+//Time fasetame;
+const now3 = new Date();
+
+console.log(now3.getTime());
+
+//start-end date;
+const start1 = new Date("2024-05-05");
+const end = new Date("2026-30-02");
+
+const diffarence = end - start1;
+
+const diffarence1 = diffarence/(1000*60*60*24);
+
+console.log(diffarence1);
+
+//find out the bd time;
+const now4 = new Date();
+
+let bdTime = now4.toLocaleDateString("en-BD");
+
+console.log(bdTime);
