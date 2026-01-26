2bb1f951fcd235a23a77696503d22e564e6fb951
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-26 22:40:47 +0600

    High Fun With JavaScript.
    Only discuss about the function.

--- highFunJavaScript.js
+++ highFunJavaScript.js
@@ -1,1 +1,16 @@
 //High Fun
+console.log("Something!");
+
+//Function;
+function functionName(){
+    console.log("Hello!");
+}
+functionName();
+
+//add with function;
+function add1(a1,b1){
+    const sum1 = a1+b1;
+    return sum1;
+};
+
+console.log(add1(4,5));
